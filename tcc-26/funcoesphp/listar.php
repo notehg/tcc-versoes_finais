62eff420b89c4b321ea91b9cde1b0fda5d889807
notehg <110551666+notehg@users.noreply.github.com>
2023-10-09 09:56:29 -0300

    Add files via upload

---
+++ tcc-26/funcoesphp/listar.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/menu-nav.css">
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Community book pleiades</title>
+</head>
+<body>
+</body>
+</html>
+
+<?php
+include 'conexao.php';
+
+// Inicializar a sessão (se já não estiver inicializada)
+session_start();
+
+// Verificar se o usuário está autenticado
+if (!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] !== true) {
+    // Se não estiver autenticado, redirecione para a página de login
+    header("Location: http://localhost:8012/tcc/index.php");
+    exit;
+}
+
+// Defina a quantidade de livros a serem exibidos por página
+$livrosPorPagina = 7;
+
+// Determine a página atual com base em um parâmetro na URL
+$pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+
+// Calcule o deslocamento para a consulta SQL
+$offset = ($pagina - 1) * $livrosPorPagina;
+
+// Consulta para obter os livros com base na página atual
+$sql = "SELECT * FROM files LIMIT $offset, $livrosPorPagina";
+$result = mysqli_query($conn, $sql);
+
+if ($result && mysqli_num_rows($result) > 0) {
+    echo "<div class='titulo'>";
+    echo "<h2>Livros Disponíveis:</h2>";
+    echo "</div>";
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $fileId = $row["id"];
+        $fileTitle = $row["title"];
+        $imageFilename = $row["image_filename"];
+
+        // Exibe a imagem como um link para a página de detalhes
+        echo "<div class='livros'>";
+        echo "<a href='det.php?id=$fileId' >";
+        echo "<img src='uploads/images/$imageFilename' alt='$fileTitle' width='100' class='img-l'>";
+        echo "</a><br>";
+        echo "Título: $fileTitle<br>";
+        echo "</div>";
+    }
+} else {
+    echo "Nenhum livro encontrado.";
+}
+
+// Crie links para navegar entre as páginas
+$totalLivros = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM files")); // Total de livros
+$totalPaginas = ceil($totalLivros / $livrosPorPagina); // Total de páginas
+
+echo "<div class='pagination'>";
+for ($i = 1; $i <= $totalPaginas; $i++) {
+    echo "<a  href='?pagina=$i'>$i</a> ";
+}
+echo "</div>";
+
+mysqli_close($conn);
+?>
---
+++ tcc-26/sql/comentario.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 28/09/2023 às 22:52
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `livraria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `cod` int(11) NOT NULL,
+  `mensagem` varchar(6000) NOT NULL,
+  `usuario_id` int(6) NOT NULL,
+  `livro_id` int(6) NOT NULL,
+  `data_publicacao` datetime DEFAULT NULL,
+  `avaliacao` int(11) NOT NULL
+) ;
+
+--
+-- Despejando dados para a tabela `comentario`
+--
+
+INSERT INTO `comentario` (`cod`, `mensagem`, `usuario_id`, `livro_id`, `data_publicacao`, `avaliacao`) VALUES
+(1, 'nostra', 15, 5, NULL, 0),
+(2, 'gay\\r\\n', 15, 5, NULL, 0),
+(3, 'gay\\r\\n', 15, 5, NULL, 0),
+(4, 'hhh', 15, 5, NULL, 0),
+(5, 'nhh', 15, 6, NULL, 0),
+(6, 'gggg', 15, 6, NULL, 0),
+(7, 'hgh', 15, 6, NULL, 0),
+(8, 'ggg', 15, 6, NULL, 0),
+(9, 'hhhh', 15, 6, NULL, 0),
+(10, 'vamos brincar\\r\\n', 15, 5, NULL, 0),
+(11, 'jjj', 15, 5, NULL, 0),
+(12, 'nbbb', 15, 5, '2023-09-24 13:43:12', 0),
+(13, 'eu', 15, 5, '2023-09-24 00:00:00', 0),
+(14, 'gay', 15, 5, '0000-00-00 00:00:00', 0),
+(15, 'nhh', 15, 5, '2023-09-24 00:00:00', 0),
+(16, 'nhh', 15, 5, '2023-09-24 14:12:12', 0),
+(17, 'fdf', 15, 5, '2023-09-24 14:47:37', 0),
+(18, 'hggg', 15, 5, '2023-09-24 14:48:48', 0),
+(19, 'gostei muito', 15, 7, '2023-09-24 14:49:29', 0),
+(20, 'm mm', 15, 5, '2023-09-28 18:48:21', 0),
+(21, '', 15, 5, '2023-09-28 20:03:15', 3),
+(22, 'nnnnn', 15, 5, '2023-09-28 20:03:19', 3),
+(23, 'ccxx', 15, 5, '2023-09-28 20:06:05', 4),
+(24, 'mjjj', 15, 5, '2023-09-28 20:07:50', 5),
+(25, '', 15, 5, '2023-09-28 20:10:18', 1),
+(26, '', 15, 5, '2023-09-28 20:42:49', 5);
+
+--
+-- Acionadores `comentario`
+--
+DELIMITER $$
+CREATE TRIGGER `check_avaliacao` BEFORE INSERT ON `comentario` FOR EACH ROW BEGIN
+    IF NEW.avaliacao > 5 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Avaliação deve ser menor ou igual a 5';
+    END IF;
+END
+$$
+DELIMITER ;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`cod`),
+  ADD KEY `livro_id` (`livro_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `cod` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`livro_id`) REFERENCES `files` (`id`),
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `logi` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
